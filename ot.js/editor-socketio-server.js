6b3dfad8e8b0e75c4656402527e2577b249106c0
Donald <448641125@qq.com>
2021-09-14 11:12:32 +0800

    update dependency

--- ot.js/editor-socketio-server.js
+++ ot.js/editor-socketio-server.js
@@ -54,7 +54,8 @@ EditorSocketIOServer.prototype.addClient = function (socket) {
       });
     })
     .on('disconnect', function () {
-      console.log("Disconnect");
+      console.log("Disconnect, docId: " + self.docId);
+
       socket.leave(self.docId);
       self.onDisconnect(socket);
       if (
